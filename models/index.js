e8f4fea5bd4a1a06a0b98ce793d7b2a560578a61
Jake Novelli <jakenovelli11@gmail.com>
2021-05-04 23:23:54 -0700

    changed back

--- models/index.js
+++ models/index.js
@@ -21,4 +21,12 @@ Game.belongsTo(User, {
     onDelete: 'CASCADE'
 });
 
+Game.hasMany(Continent, {
+    foreignKey: 'continent_id'
+});
+
+Continent.belongsTo(Game, {
+    foreignKey: 'continent_id'
+});
+
 module.exports = { User, Countries, Continent, Game };
