c097f307bfe114a20522e39441eb2379d0df2deb
markeezy <markkhoo95@gmail.com>
2021-05-06 01:16:42 -0700

    clean codepen code

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -24,16 +24,16 @@ const testObject = {
 function mainGame(continent_id) {
     // Fetch Game Data
     fetch(`/gamepage/${continent_id}`, { "method": "GET" })
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
 
-        console.log(data);
+            console.log(data);
 
-        // Start Game
+            // Start Game
 
-    });
+        });
 };
 
 // RenderGroup
@@ -100,8 +100,8 @@ const testObject = {
     ]
 };
 // Global Pointers
-const capDisplay =  document.getElementById("capitalContainer");
-const cList =       document.getElementById("countryContainer");
+const capDisplay = document.getElementById("capitalContainer");
+const cList = document.getElementById("countryContainer");
 const startButton = document.getElementById("buttonGroup");
 const scoreButton = document.getElementById("endScreen");
 
@@ -116,145 +116,150 @@ let stopTime = 0;
 
 // Test Game Start
 document.getElementById("game-7").addEventListener("click", function (event) {
-  event.preventDefault();
-  scoreButton.innerHTML = "";
-  gameStarted = true;
-  score = 0;
-  stopTime = 0;
-  displayButton(false);
-  displayEndScreen(false);
-  renderGroup(testObject);
+    event.preventDefault();
+    onStartGame();
+    renderGroup(testObject);
 });
 
 // INIT
 init();
 function init() {
-  displayButton(true);
-  displayEndScreen(false);
+    displayButton(true);
+    displayEndScreen(false);
+};
+
+// On Start Game Button click
+function onStartGame() {
+    scoreButton.innerHTML = "";
+    gameStarted = true;
+    score = 0;
+    stopTime = 0;
+    displayButton(false);
+    displayEndScreen(false);
 };
 
 // Show and Hide Start Button
-function displayButton(displayed){
-  if(displayed === true) {
-    startButton.style.setProperty('display', 'initial');
-  } else if (displayed === false) {
-    startButton.style.setProperty('display', 'none');
-  };
+function displayButton(displayed) {
+    if (displayed === true) {
+        startButton.style.setProperty('display', 'initial');
+    } else if (displayed === false) {
+        startButton.style.setProperty('display', 'none');
+    };
 };
 
 // Show and Hide End Screen
-function displayEndScreen(displayed){
-    if(displayed === true) {
-    scoreButton.style.setProperty('display', 'initial');
-  } else if (displayed === false) {
-    scoreButton.style.setProperty('display', 'none');
-  };
+function displayEndScreen(displayed) {
+    if (displayed === true) {
+        scoreButton.style.setProperty('display', 'initial');
+    } else if (displayed === false) {
+        scoreButton.style.setProperty('display', 'none');
+    };
 };
 
 // Render Group
 function renderGroup(quizData) {
-  
-  shuffleCapitals(quizData);
-  renderCountries(quizData);
-  
-  // Display First Capital
-  capitalIndex = 0;
-  renderCapital();
 
-  // Start Timer
-  gameTimer();
+    shuffleCapitals(quizData);
+    renderCountries(quizData);
+
+    // Display First Capital
+    capitalIndex = 0;
+    renderCapital();
+
+    // Start Timer
+    gameTimer();
 };
 
 // Randomize Capitals
-function shuffleCapitals (data){
-  // Generate Random Sequence for displaying Capitals
-  randSeq = [];
-  for(let j = 0; j < data.countries.length; j++) {
-    randSeq.push(j);
-  };
-  randSeq.sort(function(a, b){return 0.5 - Math.random()});
-  // Generate Randomized Capitals
-  randomizedCapitals = [];
-  for(let k = 0; k < data.countries.length; k++) {
-    randomizedCapitals.push(data.countries[randSeq[k]]);
-  };
+function shuffleCapitals(data) {
+    // Generate Random Sequence for displaying Capitals
+    randSeq = [];
+    for (let j = 0; j < data.countries.length; j++) {
+        randSeq.push(j);
+    };
+    randSeq.sort(function (a, b) { return 0.5 - Math.random() });
+    // Generate Randomized Capitals
+    randomizedCapitals = [];
+    for (let k = 0; k < data.countries.length; k++) {
+        randomizedCapitals.push(data.countries[randSeq[k]]);
+    };
 };
 
 // Render Capital
-function renderCapital (){
-  capDisplay.innerHTML = "";
-  // Display Current Capital
-  if(randomizedCapitals[capitalIndex] != undefined){
-    let p = document.createElement("h3");
-    p.textContent = randomizedCapitals[capitalIndex].capital;
-    p.setAttribute("class", "capitalDisplay");
-    p.setAttribute("data-id", randomizedCapitals[capitalIndex].id);
-    capDisplay.appendChild(p);
-    gameCapID = randomizedCapitals[capitalIndex].id;
-  } else {
-    gameStarted = false;
-    displayButton(true);
-    cList.innerHTML = "";
-    // Render End Game Screen Here
-    gameEnd();
-  };
+function renderCapital() {
+    capDisplay.innerHTML = "";
+    // Display Current Capital
+    if (randomizedCapitals[capitalIndex] != undefined) {
+        let p = document.createElement("h3");
+        p.textContent = randomizedCapitals[capitalIndex].capital;
+        p.setAttribute("class", "capitalDisplay");
+        p.setAttribute("data-id", randomizedCapitals[capitalIndex].id);
+        capDisplay.appendChild(p);
+        gameCapID = randomizedCapitals[capitalIndex].id;
+    } else {
+        gameStarted = false;
+        displayButton(true);
+        cList.innerHTML = "";
+        // Render End Game Screen Here
+        gameEnd();
+    };
 };
 
 // Render Countries
-function renderCountries (data){
-  // Create List Elements
-  for(let i = 0; i < data.countries.length; i++) {
-    let li = document.createElement("li");
-    li.textContent = data.countries[i].name;
-    li.setAttribute("class", "countryButton");
-    li.setAttribute("data-id", data.countries[i].id);
-    li.onclick = countryClicked;
-    cList.appendChild(li);
-  };
+function renderCountries(data) {
+    // Create List Elements
+    for (let i = 0; i < data.countries.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = data.countries[i].name;
+        li.setAttribute("class", "countryButton");
+        li.setAttribute("data-id", data.countries[i].id);
+        li.onclick = countryClicked;
+        cList.appendChild(li);
+    };
 };
 
 // Cycle Capitals and Score Calculate
 function countryClicked() {
-  let countryID = parseInt(this.getAttribute("data-id"));
+    let countryID = parseInt(this.getAttribute("data-id"));
 
-  if(countryID === gameCapID) {
-    score += 1;
-    console.log(score);
-  } else {
-    console.log(score);
-  };
-  
-  // Render Capital after score is calculated
-  capitalIndex += 1;
-  renderCapital();
+    if (countryID === gameCapID) {
+        score += 1;
+        console.log(score);
+    } else {
+        console.log(score);
+    };
+
+    // Render Capital after score is calculated
+    capitalIndex += 1;
+    renderCapital();
 };
 
 // Game Timer
 function gameTimer() {
-  let timeInterval = setInterval(function(){
-    stopTime += 10;
-    if(!gameStarted){
-      console.log(stopTime);
-      clearInterval(timeInterval);
-    };
-  },10);
+    let timeInterval = setInterval(function () {
+        stopTime += 10;
+        if (!gameStarted) {
+            console.log(stopTime);
+            clearInterval(timeInterval);
+        };
+    }, 10);
 };
 
 // End Screen and Submit Score
 function gameEnd() {
-  let final = document.createElement("button");
-  final.innerHTML = "Submit Score"
-  final.setAttribute("type", "button");
-  final.onclick = hideOnClick;
-  scoreButton.appendChild(final);
-  
-  displayEndScreen(true);
+    let final = document.createElement("button");
+    final.innerHTML = "Submit Score"
+    final.setAttribute("type", "button");
+    final.onclick = hideOnClick;
+    scoreButton.appendChild(final);
+
+    displayEndScreen(true);
 };
 
 // Hide onClick
 function hideOnClick() {
-  this.style.setProperty('display', 'none');
-  // this.parentElement.innerHTML = "";
+    this.style.setProperty('display', 'none');
+    // this.parentElement.innerHTML = "";
 };
 
 // ------ Codepen Code based on this HTML ---------------------------------
